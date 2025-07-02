9cbf8a15fd049adbf05d5cac8866763af337f66f
xavielson <xavielson0@yahoo.com>
2025-07-02 20:15:05 -0300

    nova janela de trigger

--- window.py
+++ window.py
@@ -44,7 +44,7 @@ class MainWindow(tk.Tk):
 
         self.clock_label = tk.Label(self, text=self.current_time, font=("Arial", 13))
 
-        self.add_alarm_setting_button = tk.Button(self, text="Add Alarm Setting", command=self.create_alarm_setting_window)
+        self.add_alarm_setting_button = tk.Button(self, text="Add Alarm Setting", command=self.create_settings_window)
         self.open_trigger_list_button = tk.Button(self, text="Open Trigger List", command=self.create_trigger_list_window)
         
         self.rega_vega_label.grid(row=1, column=0, sticky="w")
@@ -69,9 +69,167 @@ class MainWindow(tk.Tk):
         
         self.windows = []
         
+    def create_settings_window(self):
+
+        hour = []
+        for i in range(24):
+            if i > 9:
+                hour.append(str(i))
+            else:
+                hour.append("0" + str(i))
+        minute_second = []
+        for i in range(60):
+            if i > 9:
+                minute_second.append(str(i))
+            else:
+                minute_second.append("0" + str(i))
+
+        
+        self.settings_window = tk.Toplevel(self)
+        self.list_frame = tk.Frame(self.settings_window)
+        self.list_frame.pack()
+        
+        self.trigger_frame_label_row = tk.Frame(self.settings_window)
+        self.trigger_frame_label_row.pack()
+        self.trigger_frame_options_row = tk.Frame(self.settings_window)
+        self.trigger_frame_options_row.pack()
+        self.trigger_frame_button_row = tk.Frame(self.settings_window)
+        self.trigger_frame_button_row.pack()   
+
+        self.open_trigger_list_button["state"] = "disable"
+        
+        self.windows.append(self.settings_window)
+        
+        self.settings_window.geometry("+800+300")
+        self.settings_window.title("Lista de Triggers")
+        
+        self.trigger_listbox = tk.Listbox(self.list_frame)
+        
+        self.regavega_button = tk.Button(self.list_frame,
+                                         text="Rega Vega",
+                                         command=lambda: self.get_lista_alarmes(self.trigger_listbox, 
+                                                                                "rega_vega"))
+        self.regaflora_button = tk.Button(self.list_frame,
+                                         text="Rega Flora",
+                                         command=lambda: self.get_lista_alarmes(self.trigger_listbox, 
+                                                                                "rega_flora") )
+        self.ledvega_button = tk.Button(self.list_frame,
+                                         text="Led Vega",
+                                         command=lambda: self.get_lista_alarmes(self.trigger_listbox, 
+                                                                                "led_vega"))
+        self.ledflora_button = tk.Button(self.list_frame,
+                                         text="Led Flora",
+                                         command=lambda: self.get_lista_alarmes(self.trigger_listbox, 
+                                                                                "led_flora"))
+        self.wavemaker_button = tk.Button(self.list_frame,
+                                         text="Wavemaker",
+                                         command=lambda: self.get_lista_alarmes(self.trigger_listbox, 
+                                                                                "wavemaker"))
+        self.runoff_button = tk.Button(self.list_frame,
+                                         text=" Runoff ",
+                                         command=lambda: self.get_lista_alarmes(self.trigger_listbox, 
+                                                                                "runoff")) 
+            #label
+        create_alarm_label = tk.Label(self.trigger_frame_options_row, 
+                                      text="    ", 
+                                      font=("Arial", 10))
+
+        #relay combobox
+        relay_list = tk.StringVar()
+        relay_combobox = ttk.Combobox(self.trigger_frame_options_row, 
+                                      textvariable=relay_list, 
+                                      width=9)        
+        relay_combobox['values'] = data_manager.relays
+        relay_combobox['state'] = 'readonly'
+
+        hour_list = tk.StringVar()
+        hour_combobox = ttk.Combobox(self.trigger_frame_options_row, 
+                                     textvariable=hour_list, 
+                                     width=3)
+        hour_combobox["values"] = hour
+        
+        minute_list = tk.StringVar()
+        minute_combobox = ttk.Combobox(self.trigger_frame_options_row, 
+                                       textvariable=minute_list, 
+                                       width=3)
+        minute_combobox["values"] = minute_second
+        
+        second_list = tk.StringVar()
+        second_combobox = ttk.Combobox(self.trigger_frame_options_row, 
+                                       textvariable=second_list, 
+                                       width=3)
+        second_combobox["values"] = minute_second 
+
+        #onoff radiobutton
+        selected_status_button = tk.BooleanVar()
+
+        on = ttk.Radiobutton(self.trigger_frame_options_row, 
+                             text="On", 
+                             variable=selected_status_button, 
+                             value=True)
+        
+        off = ttk.Radiobutton(self.trigger_frame_options_row, 
+                              text="Off", 
+                              variable=selected_status_button, 
+                              value=False)        
+
+        #labels
+        relay_label = tk.Label(self.trigger_frame_label_row, 
+                               text="Relay", 
+                               font=("Arial", 13),
+                               width=10)
+        
+        time_label = tk.Label(self.trigger_frame_label_row, 
+                              text="Time", 
+                              font=("Arial", 13),
+                              width=10)
+        
+        status_label = tk.Label(self.trigger_frame_label_row, 
+                                text="Status", 
+                                font=("Arial", 13),
+                                width=10)
+        
+        #add alarm button
+        add_alarm_button = tk.Button(self.trigger_frame_button_row, 
+                                     text="Set Alarm", 
+                                     command=lambda: self.add_alarm(relay_combobox.get(), 
+                                                                    hour_combobox.get(), 
+                                                                    minute_combobox.get(), 
+                                                                    second_combobox.get(), 
+                                                                    selected_status_button))
+                
+        ### TRIGGER LIST ###  
+        self.trigger_listbox.pack(side="left")
+        self.regavega_button.pack(expand=True, fill='x')
+        self.regaflora_button.pack(expand=True, fill='x')
+        self.ledvega_button.pack(expand=True, fill='x')
+        
+        self.ledflora_button.pack(expand=True, fill='x')
+        self.wavemaker_button.pack(expand=True, fill='x')
+        self.runoff_button.pack(expand=True, fill='x')
+
+        ### ADD ALARM ###
+        #create_alarm_label.pack()
+
+        relay_label.pack(side="left", expand=True, fill='x')
+        time_label.pack(side="left", expand=True, fill='x')
+        status_label.pack(side="left", expand=True, fill='x')
+
+        relay_combobox.pack(side="left")
+        hour_combobox.pack(side="left")
+        minute_combobox.pack(side="left")
+        second_combobox.pack(side="left")
+        on.pack(side="left")
+        off.pack(side="left")
+
+        add_alarm_button.pack(expand=True, fill='x')
+
+        self.settings_window.protocol("WM_DELETE_WINDOW", self.close_trigger_list_window)
+
     def create_trigger_list_window(self):
         
 #         global trigger_list_window
+
         self.trigger_list_window = tk.Toplevel(self)
         self.list_frame = tk.Frame(self.trigger_list_window)
         self.list_frame.pack()
@@ -85,42 +243,39 @@ class MainWindow(tk.Tk):
         
         self.windows.append(self.trigger_list_window)
         
-        self.trigger_list_window.geometry("315x315+810+300")
+        self.trigger_list_window.geometry("+800+300")
         self.trigger_list_window.title("Lista de Triggers")
         
         self.trigger_listbox = tk.Listbox(self.list_frame)
         
-        self.regavega_button = tk.Button(self.row1_buttons_frame,
+        self.regavega_button = tk.Button(self.list_frame,
                                          text="Rega Vega",
-                                         command=self.create_trigger_list_window)
-        self.regaflora_button = tk.Button(self.row1_buttons_frame,
+                                         command=lambda: self.get_lista_alarmes(self.trigger_listbox, "rega_vega"))
+        self.regaflora_button = tk.Button(self.list_frame,
                                          text="Rega Flora",
-                                         command=self.create_trigger_list_window)
-        self.ledvega_button = tk.Button(self.row1_buttons_frame,
+                                         command=lambda: self.get_lista_alarmes(self.trigger_listbox, "rega_flora") )
+        self.ledvega_button = tk.Button(self.list_frame,
                                          text="Led Vega",
-                                         command=self.create_trigger_list_window)
-        self.ledflora_button = tk.Button(self.row2_buttons_frame,
+                                         command=lambda: self.get_lista_alarmes(self.trigger_listbox, "led_vega"))
+        self.ledflora_button = tk.Button(self.list_frame,
                                          text="Led Flora",
-                                         command=self.create_trigger_list_window)
-        self.wavemaker_button = tk.Button(self.row2_buttons_frame,
+                                         command=lambda: self.get_lista_alarmes(self.trigger_listbox, "led_flora"))
+        self.wavemaker_button = tk.Button(self.list_frame,
                                          text="Wavemaker",
-                                         command=self.create_trigger_list_window)
-        self.runoff_button = tk.Button(self.row2_buttons_frame,
+                                         command=lambda: self.get_lista_alarmes(self.trigger_listbox, "wavemaker"))
+        self.runoff_button = tk.Button(self.list_frame,
                                          text=" Runoff ",
-                                         command=self.create_trigger_list_window) 
-
+                                         command=lambda: self.get_lista_alarmes(self.trigger_listbox, "runoff")) 
+                
+        #self.get_lista_alarmes(self.trigger_listbox)  
+        self.trigger_listbox.pack(side="left")
+        self.regavega_button.pack(expand=True, fill='x')
+        self.regaflora_button.pack(expand=True, fill='x')
+        self.ledvega_button.pack(expand=True, fill='x')
         
-        self.get_lista_alarmes(self.trigger_listbox)  
-        self.trigger_listbox.pack()
-        self.regavega_button.pack(side="right")
-        self.regaflora_button.pack(side="right")
-        self.ledvega_button.pack(side="right")
-        
-        self.ledflora_button.pack(side="right")
-        self.wavemaker_button.pack(side="right")
-        self.runoff_button.pack(side="right", fill="x", expand=True)
-        
-        
+        self.ledflora_button.pack(expand=True, fill='x')
+        self.wavemaker_button.pack(expand=True, fill='x')
+        self.runoff_button.pack(expand=True, fill='x')       
         
         self.trigger_list_window.protocol("WM_DELETE_WINDOW", self.close_trigger_list_window)
     
@@ -139,8 +294,34 @@ class MainWindow(tk.Tk):
                 status = "Off"
             listbox.insert(x, alarme.relay_str+" "+alarme.time+" "+status)
             x = x+1        
+
+    def get_lista_alarmes(self, listbox, relay):
+        self.trigger_listbox.delete(0, "end")  # Clear the listbox before inserting new items         
+        x = 1
+        for alarme in data_manager.lista_alarmes:
+            if alarme.status == 1:
+                status = "On"
+            elif alarme.status == 0:
+                status = "Off"
+            if alarme.relay_str == relay:
+                listbox.insert(x, alarme.relay_str+" "+alarme.time+" "+status)
+                x = x+1
+            else:
+                pass     
         
     def create_alarm_setting_window(self):    
+        hour = []
+        for i in range(24):
+            if i > 9:
+                hour.append(str(i))
+            else:
+                hour.append("0" + str(i))
+        minute_second = []
+        for i in range(60):
+            if i > 9:
+                minute_second.append(str(i))
+            else:
+                minute_second.append("0" + str(i))
 
         self.alarm_setting_window = tk.Toplevel(self)
         self.add_alarm_setting_button["state"] = "disable"
@@ -157,20 +338,7 @@ class MainWindow(tk.Tk):
         relay_combobox = ttk.Combobox(self.alarm_setting_window, textvariable=relay_list, width=9)        
         relay_combobox['values'] = data_manager.relays
         relay_combobox['state'] = 'readonly'
-        
-        hour = []
-        for i in range(24):
-            if i > 9:
-                hour.append(str(i))
-            else:
-                hour.append("0" + str(i))
-        minute_second = []
-        for i in range(60):
-            if i > 9:
-                minute_second.append(str(i))
-            else:
-                minute_second.append("0" + str(i))
-            
+
         hour_list = tk.StringVar()
         hour_combobox = ttk.Combobox(self.alarm_setting_window, textvariable=hour_list, width=2)
         hour_combobox["values"] = hour
